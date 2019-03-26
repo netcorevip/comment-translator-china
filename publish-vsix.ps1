d890480a18e5c1aab0d476595de79c71115c693b
JiaWork <JiaWork@outlook.com>
2019-03-26 18:19:12 +0800

    添加自动化脚本

---
+++ publish-vsix.ps1
@@ -0,0 +1,19 @@
+$vsixpublish = Get-ChildItem -File .\packages -recurse | 
+    Where-Object { $_.Name -eq "VsixPublisher.exe" } | 
+    Sort-Object -Descending -Property CreationTime | 
+    Select-Object -First 1 -ExpandProperty FullName
+
+#发布账号登陆
+. $vsixpublish login -publisherName thealexdresko -personalAccessToken $env:commenttranslatorchinapublish
+
+#相关描述文件，vsix文件路径
+$overview = (Get-Item .\overview.md).FullName
+$manifest = (Get-Item .\publish-manifest.json).FullName
+$vsix = (Get-Item .\CommentTranslator\bin\Release\CommentTranslatorChina.vsix).FullName
+Write-Host "vsix: $vsix"
+Write-Host "manifest: $manifest"
+Write-Host "overview: $overview"
+
+# . $vsixpublish deleteExtension -extensionName "HomeSeerTemplates" -publisherName "thealexdresko"
+
+. $vsixpublish publish -payload "$vsix" -publishManifest "$manifest"
