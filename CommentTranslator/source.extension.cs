c000e74007bb18b930c0a4d2b0c71be295a92117
JiaWork <JiaWork@outlook.com>
2019-03-30 03:25:52 +0800

    update ID

--- CommentTranslator/source.extension.cs
+++ CommentTranslator/source.extension.cs
@@ -7,7 +7,7 @@ namespace CommentTranslatorChina
 {
     static class Vsix
     {
-        public const string Id = "9EE09180-C729-4369-9E1B-4FEAB1C3C0D0";
+        public const string Id = "4E1009A3-D979-453E-B492-7381603678F6";
         public const string Name = "Comment Translator China";
         public const string Description = @"Extension of visual studio that automatic translate comment in you open file.";
         public const string Language = "zh-CN";
